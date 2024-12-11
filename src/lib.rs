1f659b995928e643f45d2961c5e80415f5e4cf5f
Timo Betcke <timo.betcke@gmail.com>
2024-12-11 21:26:26 +0000

    Fixed visibility

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@
 #![cfg_attr(feature = "strict", deny(warnings), deny(unused_crate_dependencies))]
 #![warn(missing_docs)]
 
-mod ghost_communicator;
-mod index_layout;
+pub mod ghost_communicator;
+pub mod index_layout;
 
 pub use ghost_communicator::GhostCommunicator;
 pub use index_layout::{DefaultDistributedIndexLayout, IndexLayout};
