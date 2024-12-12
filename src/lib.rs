9af45eaedb98b5410692bcaaffaae69fff4bdc04
Timo Betcke <timo.betcke@gmail.com>
2024-12-12 11:13:24 +0000

    Added array tools

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 #![cfg_attr(feature = "strict", deny(warnings), deny(unused_crate_dependencies))]
 #![warn(missing_docs)]
 
+pub mod array_tools;
 pub mod ghost_communicator;
 pub mod index_layout;
 
