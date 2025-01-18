52e41ae3bb22f613baffe91a1ffaa6d6081fa64c
Timo Betcke <timo.betcke@gmail.com>
2025-01-18 16:36:20 +0000

    Renamed data mapper (#12)

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,12 @@
 #![warn(missing_docs)]
 
 pub mod array_tools;
-pub mod dof_mapper;
+pub mod data_mapper;
 pub mod ghost_communicator;
 pub mod index_layout;
 pub mod permutation;
 
+pub use data_mapper::Global2LocalDataMapper;
 pub use ghost_communicator::GhostCommunicator;
 pub use index_layout::IndexLayout;
+pub use permutation::DataPermutation;
